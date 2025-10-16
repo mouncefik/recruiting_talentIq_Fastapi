4ff973f67c54df456a156ebfbfbd00a4300e65d5
mouncef <mouncefik@gmail.com>
2025-10-16 14:26:30 +0100

    Add project structures

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from core.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="TalentIQ")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to TalentIQ"}
